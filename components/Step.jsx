782c669416e7494092928808545a5415241e7ec1
Ricardo Poullet <rickypoullet@gmail.com>
2022-04-01 15:22:37 -0400

    Finished Scheduling page, need to fix gif sizing.

--- components/Step.jsx
+++ components/Step.jsx
@@ -1,6 +1,6 @@
 function ScheduleStep({ info, number }) {
   return (
-    <div className="flex items-center space-x-3">
+    <div className="flex items-center space-x-3 w-full">
       <div className="p-4 md:p-6 bg-black rounded-full overflow-hidden relative mb-auto">
         <h3 className="text-white absolute top-1/2 left-1/2 mt-[2px] -translate-x-1/2 -translate-y-1/2">
           {number}
--- pages/schedule.js
+++ pages/schedule.js
@@ -1,10 +1,10 @@
 import Head from "next/head";
-import { useEffect } from "react";
-import { BookingsApi, Client } from "square";
+import Image from "next/image";
 import LeftLine from "../components/LeftLine";
 import Navbar from "../components/Navbar";
 import PageHeader from "../components/PageHeader";
 import ScheduleStep from "../components/Step";
+import { motion } from "framer-motion";
 
 function Schedule() {
   return (
@@ -20,58 +20,90 @@ function Schedule() {
         <LeftLine />
         <section className="text-custom-black">
           <PageHeader title={"Schedule"} />
-          <div className="text-center mt-4">
-            <p>
+
+          <h2 className="my-2 sm:block xs:my-8 text-center">
+            Please follow the steps below
+          </h2>
+
+          <div className="flex flex-col sm:flex-row-reverse gap-5 items-center justify-between">
+            <div className="relative w-full h-[200px] sm:h-[50vh] sm:max-w-[50%]">
+              <Image
+                src={"/gifs/scheduling-instructions.gif"}
+                layout="fill"
+                objectFit="contain"
+                alt=""
+              />
+            </div>
+
+            <div>
+              <div className="my-2 xs:my-5 flex flex-col space-y-2">
+                <ScheduleStep
+                  number={1}
+                  info={
+                    <p className="step-text">
+                      Click on a drop down menu that displays{" "}
+                      <strong>&quot;All Providers&quot;</strong>
+                    </p>
+                  }
+                />
+                <ScheduleStep
+                  number={2}
+                  info={
+                    <p className="step-text">
+                      Select <strong>&quot;Alexa&quot;</strong>
+                    </p>
+                  }
+                />
+                <ScheduleStep
+                  number={3}
+                  info={
+                    <p className="step-text">
+                      Click <strong>&quot;Add to visit&quot;</strong> for your
+                      desired service.
+                    </p>
+                  }
+                />
+                <ScheduleStep
+                  number={4}
+                  info={
+                    <p className="step-text">
+                      Select <strong>&quot;Alexa&quot;</strong> as a provider.
+                    </p>
+                  }
+                />
+                <ScheduleStep
+                  number={5}
+                  info={
+                    <p className="step-text">
+                      Click <strong>&quot;Choose date and time&quot;</strong> to
+                      schedule
+                    </p>
+                  }
+                />
+              </div>
+            </div>
+          </div>
+          <div className="flex justify-center flex-col items-center mb-5">
+            <motion.a
+              className="button w-full sm:w-60 mx-auto py-4 sm:py-6 text-center"
+              href="https://www.schedulicity.com/scheduling/tcw3sb/services"
+              target="_blank"
+              rel="noreferrer"
+              whileHover={{
+                scale: 1.05,
+                boxShadow: "0px 5px 11px -3px rgba(0,0,0,0.5)",
+                transition: {
+                  duration: 0.5,
+                  ease: [0.6, 0.01, -0.05, 0.95],
+                },
+              }}
+            >
+              Okay
+            </motion.a>
+            <p className="mt-1 text-sm italic text-center max-w-[250px]">
               <strong>You will be redirected to Schedulicity</strong> to
               schedule your appointment with Alexa!
             </p>
-            <p className="mt-3">Please follow the steps below</p>
-          </div>
-
-          <div className="mt-10 space-y-2">
-            <ScheduleStep
-              number={1}
-              info={
-                <p>
-                  Click on a drop down menu that displays{" "}
-                  <strong>&quot;All Providers&quot;</strong>
-                </p>
-              }
-            />
-            <ScheduleStep
-              number={2}
-              info={
-                <p>
-                  Select <strong>&quot;Alexa&quot;</strong>
-                </p>
-              }
-            />
-            <ScheduleStep
-              number={3}
-              info={
-                <p>
-                  Click <strong>&quot;Add to visit&quot;</strong> for your
-                  desired service.
-                </p>
-              }
-            />
-            <ScheduleStep
-              number={4}
-              info={
-                <p>
-                  Select <strong>&quot;Alexa&quot;</strong> as a provider.
-                </p>
-              }
-            />
-            <ScheduleStep
-              number={5}
-              info={
-                <p>
-                  Click <strong>&quot;Choose date and time&quot;</strong> to
-                  schedule
-                </p>
-              }
-            />
           </div>
         </section>
       </main>
